5bcb7118d9a6a1662ad33176f38f86efe4b29410
Uthistran Selvaraj <uthistran@gmail.com>
2020-09-01 06:32:33 -0700

    adding build

---
+++ menudisplay/build/precache-manifest.b771e7a354934c5163cf2cc77bda4b5b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05687d30f042f11170b45ff052009594",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8756a920509dd58839e7",
+    "url": "./static/css/main.c2753c38.chunk.css"
+  },
+  {
+    "revision": "20197e5cdf0785b25fab",
+    "url": "./static/js/2.56f410bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.56f410bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8756a920509dd58839e7",
+    "url": "./static/js/main.d743cd38.chunk.js"
+  },
+  {
+    "revision": "f983e67ec9910003b087",
+    "url": "./static/js/runtime-main.77f135e6.js"
+  },
+  {
+    "revision": "fb90e8124e2fa1d81d8c674f15abb0f9",
+    "url": "./static/media/appletree.fb90e812.svg"
+  },
+  {
+    "revision": "e25b8045f1174116ab36488feb8a9433",
+    "url": "./static/media/logo.e25b8045.svg"
+  },
+  {
+    "revision": "36fd4ceff1ee7935c13bfa59228ca3e8",
+    "url": "./static/media/up.36fd4cef.svg"
+  }
+]);
